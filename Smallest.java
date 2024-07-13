934bc362f9f9810e32a4191521d20730d3c4dc56
rithvika070 <160831599+rithvika070@users.noreply.github.com>
2024-07-13 10:28:02 +0530

    Add files via upload

---
+++ Smallest.java
@@ -0,0 +1,19 @@
+class Smallest{
+	public static void main(String[] args){
+		int a=8,b=3,c=10;
+		if(a<b)
+		{
+			if(a<c){
+				System.out.print("Smallest number is"+a);
+			}
+			else{
+				System.out.print("Smallest number is"+c);
+			}
+		}
+		else{
+			System.out.print("Smallest number is"+b);
+		}
+	
+	}
+}
+
